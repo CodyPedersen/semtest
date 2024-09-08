fb1b67e38a074a44c5169d82b405fb75ca40b22b
CodyPedersen <cody.pedersen@proton.me>
2024-09-08 17:42:12 -0400

    chore: remove unnecessary abstraction from runtime

--- semtest/__init__.py
+++ semtest/__init__.py
@@ -1,14 +1,13 @@
 """Core imports for semtest library functionality"""
 from .benchmarking import Benchmark, benchmark
 from .llm_client import EmbeddingClient
-from .semantic_comparator import SemanticComparator, CosineSimilarity
+from .semantic_comparator import CosineSimilarity
 from .semtest import semantic_test_runner
 
 __all__ = [
     "Benchmark",
     "EmbeddingClient",
     "CosineSimilarity",
-    "SemanticComparator",
-    "benchmarking",
+    "benchmark",
     "semantic_test_runner"
 ]
--- semtest/semantic_comparator/__init__.py
+++ semtest/semantic_comparator/__init__.py
@@ -1,6 +1,5 @@
 """Functionality for comparison of embedding vectors"""
 
-from .semantic_comparator import SemanticComparator
 from .comparators import ComparatorBase, CosineSimilarity
 
-__all__ = ["ComparatorBase","CosineSimilarity", "SemanticComparator"]
+__all__ = ["ComparatorBase","CosineSimilarity"]
