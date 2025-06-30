1e250e07977cf0c63d415442db71c065526f0725
CodyPedersen <cody.pedersen@proton.me>
2025-06-30 00:15:34 -0400

    test pylint pytest actions fix

---
+++ example_benchmarks/external_prompt.py
@@ -0,0 +1,11 @@
+# Mocks a highly simplified external prompt or function call
+
+TEST_SYSTEM_PROMPT = (
+    "You are an expert at examining IT controls and answer questions in a succinct "
+    "fashion (ideally in one sentence)"
+)
+
+TEST_USER_PROMPT = (
+    "Based on the following dataset of folder privileges, who are the most "
+    "likely administrators\n```json\n{sample_data}\n```"
+)
